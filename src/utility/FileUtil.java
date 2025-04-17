3eb1dfbbc41a7269dce041cbe21af621ac91fa49
Halol12332 <prajnajaya05@gmail.com>
2025-04-18 00:49:40 +0800

    first commit

---
+++ src/utility/FileUtil.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utility;
+
+/**
+ *
+ * @author user
+ */
+public class FileUtil {
+    /*
+    public static LinkedList<JobPosting> readJobsFromFile(String filePath) {
+        LinkedList<JobPosting> jobs = new LinkedList<>();
+        // Read from file and populate the list
+        return jobs;
+    }
+    */
+}
